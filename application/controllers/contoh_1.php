94fb743a5d4ee95412cf7695150746941d4b3648
anisarahma10 <anisarma1013@gmail.com>
2022-04-21 09:02:59 +0700

    pustaka-booking

---
+++ application/controllers/contoh_1.php
@@ -0,0 +1,13 @@
+<?php
+
+class contoh_1 extends CI_Controller {
+public function index()
+	{
+		echo "Biodata Saya<br>";
+		echo "Nama Lengkap : Anisa Rahma<br>";
+		echo "Nim : 17190203<br>";
+		echo "Fakultas : Teknik Informatika<br>";
+		echo "Prodi : Teknologi Informasi<br>";
+		echo "Jenis Kelamin : Perempuan";
+	}
+}
